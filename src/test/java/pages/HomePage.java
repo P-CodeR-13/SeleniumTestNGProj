7b7c7ea72c0c8055f4051ab8db830de1490a0dd0
P-CodeR-13 <paolo.retoran13@gmail.com>
2024-10-25 17:30:45 +0800

    Optimized Code

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,17 +1,16 @@
 package pages;
 
 import org.openqa.selenium.By;
-import org.testng.Assert;
 
 import static Tests.SeleniumTest.driver;
 
 public class HomePage {
 
-    public static String home_url = "https://demoqa.com/";
-    public static String elements_menu = "//h5[normalize-space()='Elements']";
+    public static String homeUrl = "https://demoqa.com/";
+    public static String elementsMenu = "//h5[normalize-space()='Elements']";
 
 
     public static void goToElementsMenu() {
-        driver.findElement(By.xpath(elements_menu)).click();
+        driver.findElement(By.xpath(elementsMenu)).click();
     }
 }
